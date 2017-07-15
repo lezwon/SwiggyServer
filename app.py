28875e6417ae5fefac7b4e7c85f804890b947c9b
lezwon <lezwon@gmail.com>
2017-07-15 23:57:59 +0530

    Added LatLon

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
+from geopy.distance import vincenty
 import requests
+import LatLon
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+conn = psycopg2.connect(
+	dbname="dq82dh5e9an5",
+    user="jjmfzenixiiqri",
+    password="5838f7538a792efcf4b8fd7e263ec720fcca4870d06fa86427d5c629ca508af7",
+    host="ec2-50-19-83-146.compute-1.amazonaws.com",
+    port="5432"
+    )
 
 @app.route('/search')
 @cross_origin()
@@ -27,7 +37,59 @@ def search_items():
 	print url
 	return res.text
 
+insert_into_table(user,lat,lon):
+	# Connect to an existing database
+
+	lat = float(lat)
+	lon = float(lon)
+
+	# Open a cursor to perform database operations
+	cur = conn.cursor()
+
+	# Execute a command: this creates a new table
+	cur.execute("CREATE TABLE location IF NOT EXISTS (id serial PRIMARY KEY, lat decimal, lon decimal);")
+
+	# Pass data to fill a query placeholders and let Psycopg perform
+	# the correct conversion (no more SQL injections!)
+	cur.execute("INSERT INTO location (id, lat, lon) VALUES (%d, %f, %f)",(user, lat, lon))
+
+	# Make the changes to the database persistent
+	conn.commit()
+
+	# Close communication with the database
+	cur.close()
+
+
+query_from_table(lat,lon):
+	# Connect to an existing database
+	lat = float(lat)
+	lon = float(lon)
+
+	currentPos = LatLon(lat, lon)
+	offsetTop = currentPos.offset(90, 1)
+	offsetBottom = currentPos.offset(275, 1)
+
+	offsetRight = currentPos.offset(0, 1)
+	offsetLeft = currentPos.offset(270, 1)
+	# Open a cursor to perform database operations
+	cur = conn.cursor()
+
+	cur.execute("SELECT * from location WHERE lat in between "+offsetTop+" and "+offsetBottom +"and lon in between "+offsetRight+" and "+offsetLeft)
+	rows = cur.fetchall()
+	for row in rows:
+    	print "   ", row[1][1]
+	# Close communication with the database
+	cur.close()
+
+
+@app.route('/setlocation', methods=['POST'])
+def set_location():
+	lat = request.args['lat']
+	lon = int(request.args['lon'])
+	insert_into_table(user,lat,lon)
+	return "Your location has been successfully set"
+
 if __name__ == '__main__':
     app.run()
 
-    # https://www.swiggy.com/api/restaurants/search?third_party_vendor=1&str=mutton&lat=12.9345625&lng=77.60613179999996&page=
+ 
