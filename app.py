9f52ac3a2a9a08e7588e3af27ee642d710ccc494
lezwon <lezwon@gmail.com>
2017-07-15 17:13:57 +0530

    search products

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask
+from flask import request
+import requests
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/search')
+def search_items():
+	dish = request.args['dish']
+	budget = int(request.args['budget'])
+	cost = None
+	if(budget < 500):
+		cost='$'
+	elif(budget < 1000):
+		cost='$$'
+	elif(budget < 2000):
+		cost='$$$'
+	else:
+		cost='$$$$'
+
+	url = 'https://www.swiggy.com/api/restaurants/search?third_party_vendor=1&str='+dish+'&lat=12.9345625&lng=77.60613179999996&cost_for_two='+cost
+	res = requests.get(url)
+	print url
+	return res.text
 
 if __name__ == '__main__':
     app.run()
+
+    # https://www.swiggy.com/api/restaurants/search?third_party_vendor=1&str=mutton&lat=12.9345625&lng=77.60613179999996&page=
